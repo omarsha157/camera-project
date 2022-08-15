be7928000f0447edc4e2dba87a55b40f0c46da5f
omarsha157 <omarsha157@gmail.com>
2022-08-15 20:47:06 +0530

    layout update

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ function navSlide() {
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        burger.classList.toggle('burger-close');
     })
 }
 
 function navClose() {
-
+    const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navlinks = document.querySelectorAll('.nav-links li a')
     navlinks.forEach(link => 
         link.addEventListener('click', () => {
             nav.classList.remove('nav-active');
+            burger.classList.remove('burger-close');
         })
         )
 }
